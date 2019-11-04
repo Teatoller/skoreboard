556527e431843a90674a818aaee9d33b67d1f743
Teatoller <steven.otieno1@gmail.com>
2019-11-04 09:39:20 +0300

    (uplift state): Implement unidirection state flow
    - change counter to stateless functional component
    - add props to Counter componet
    - add props to player component
    - add score to initial state array
    - add event handlescorechange on app
    - add changeScore props on player
    - add onClick events on counter button
    - add changeScore props on Counter component
    - add index argument to player.
    - add index props to player and counter
    - update score based on player index
    
    [Starts #004]

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,65 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Player from './Player';
+import React, { Component } from "react";
+import Header from "./Header";
+import Player from "./Player";
 
-
-  
 class App extends Component {
   state = {
     players: [
       {
         name: "Steven",
+        score: 0,
         id: 1
       },
       {
         name: "Denzel",
+        score: 0,
         id: 2
       },
       {
         name: "Nigel",
+        score: 0,
         id: 3
       },
       {
         name: "Fay",
+        score: 0,
         id: 4
       }
     ]
   };
 
-  handleRemovePlayer = (id) => {
-    this.setState( prevState => {
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: (prevState.players[index].score += delta)
+    }));
+    // console.log("index:" + index , "delta:" + delta);
+  };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
       };
     });
-  }
+  };
 
   render() {
     return (
       <div className="scoreboard">
-        <Header 
-          title="Scoreboard" 
-          totalPlayers={this.state.players.length} 
-        />
-  
+        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+
         {/* Players list */}
-        {this.state.players.map( player =>
-          <Player 
+        {this.state.players.map((player, index) => (
+          <Player
             name={player.name}
             id={player.id}
-            key={player.id.toString()} 
-            removePlayer={this.handleRemovePlayer}           
+            score={player.score}
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
           />
-        )}
+        ))}
       </div>
     );
   }
